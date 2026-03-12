46a8735c25ec018608a929e7f7bc70223e627e8a
Ömer Faruk Çelik <omerfaruk.celik@partner.turktelekom.com.tr>
2026-03-12 13:36:47 +0300

    Update config.js with 8 team member buttons

--- config.js
+++ config.js
@@ -5,12 +5,15 @@
  * Update BUTTON_LABELS to set the text displayed on each button.
  */
 
-const NUM_BUTTONS = 5;
+const NUM_BUTTONS = 8;
 
 const BUTTON_LABELS = [
-  "Button 1",
-  "Button 2",
-  "Button 3",
-  "Button 4",
-  "Button 5",
+  "OSMAN YUSUF TOSUN",
+  "MERT EREN KAPLAN",
+  "KEMAL MAHMUTOĞULLARI",
+  "AYSU KESKİN",
+  "SEZİN DOĞAN",
+  "AHMET ÇAĞDAŞ GİRİT",
+  "OĞUZ SEMİH ARTIK",
+  "ÖMER FARUK ÇELİK",
 ];
