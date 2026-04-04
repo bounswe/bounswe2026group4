d725c56fbf1b4d94bfa00aca562d6307537042bc
OsmanYusufTosun <osyustos14@gmail.com>
2026-04-04 15:13:15 +0300

    Revert "bug: reload the empty sign in page after a user logins"
    This reverts commit bb6f4b279cd9a691fb342251124db167a7fcd107.

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -7,7 +7,7 @@ DEBUG = True
 
 SECRET_KEY = env('SECRET_KEY')  # noqa: F405
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1' , '10.1.204.33']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Longer token lifetime in dev for convenience
 SIMPLE_JWT = {
