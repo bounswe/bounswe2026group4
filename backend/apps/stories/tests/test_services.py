d8caf7ceca6555f0f55638cf2e31b11c736708c0
AHMET-CAGDAS-GIRIT <141752766+AHMET-CAGDAS-GIRIT@users.noreply.github.com>
2026-04-30 23:43:39 +0300

    ok. this should work

--- backend/apps/stories/tests/test_services.py
+++ backend/apps/stories/tests/test_services.py
@@ -311,7 +311,7 @@ class TestGetStorySearch:
         s1 = make_story(title='Oldest')
         s2 = make_story(title='Middle')
         s3 = make_story(title='Newest')
-        results = list(get_story_search("dummy",sort_by='recent'))
+        results = list(get_story_search("",sort_by='recent'))
         assert results[0] == s3
         assert results[1] == s2
         assert results[2] == s1
@@ -322,7 +322,7 @@ class TestGetStorySearch:
         s1 = make_story(title='Least Popular', like_count=5)
         s2 = make_story(title='Medium Popular', like_count=10)
         s3 = make_story(title='Most Popular', like_count=20)
-        results = list(get_story_search("dummy",sort_by='popular'))
+        results = list(get_story_search("",sort_by='popular'))
         assert results[0] == s3
         assert results[1] == s2
         assert results[2] == s1
